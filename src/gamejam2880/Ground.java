087c4a5b96c8d40a2e77d0486ef213a684d4c43e
astropojke <W0292256@nscc.ca>
2017-02-23 14:31:28 -0400

    Pulls .png image
    Pull’s image  “jGround”
    
    Does not auto generate yet..

--- src/gamejam2880/Ground.java
+++ src/gamejam2880/Ground.java
@@ -1,13 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package gamejam2880;
+
 
 /**
  *
  * @author kylechapman
  */
-public class Ground {
-    
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+public class Ground extends JPanel {
+        
+        public Image jGround; 
+        
+        public Ground() {
+            initGround();
+        }
+        private void initGround(){
+            loadGround();
+            int w = jGround.getWidth(this);
+            int h = jGround.getHeight(this);
+            setPreferredSize(new Dimension(w, h));
+        }
+        
+        private void loadGround() {
+            ImageIcon ii = new ImageIcon("jGround.png");
+            jGround = ii.getImage();
+        }
+        @Override
+        public void paintComponent(Graphics g) {
+        g.drawImage(jGround, 0, 0, null);
+    }
 }
