40386ac096c0adbf627e9e66527c884a9786fc34
RobertLeCreux <robertlecreux@hotmail.com>
2017-02-23 14:02:57 -0400

    player

--- src/gamejam2880/Player.java
+++ src/gamejam2880/Player.java
@@ -4,35 +4,94 @@
  * and open the template in the editor.
  */
 package gamejam2880;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
-import java.util.*;
+public class Player extends Sprite {
 
-/**
- *
- * @author rober
- */
-public class Player extends Sprite{
-    private int dx, dy, lives;
-    private ArrayList<Rounds> rounds;
-    
-    private final int MAX_LIVES = 3;
-    
+    private int dx;
+    private int dy;
+    // private ArrayList<Missile> missiles;
+
     public Player(int x, int y) {
         super(x, y);
-        lives = MAX_LIVES;
+
+        initCraft();
     }
-    
-    public void move(){
+
+    private void initCraft() {
+        
+        // missiles = new ArrayList<>();
+        loadImage("craft.png");
+        getImageDimensions();
+    }
+
+    public void move() {
+
         x += dx;
         y += dy;
-        
-        if (x<1) x=1;
-        if (y<1) y=1;
+
+        if (x < 1) {
+            x = 1;
+        }
+
+        if (y < 1) {
+            y = 1;
+        }
     }
-    
-    public ArrayList getRounds(){
-        return rounds;
+/*
+    public ArrayList getMissiles() {
+        return missiles;
+    }*/
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+        /*
+        if (key == KeyEvent.VK_SPACE) {
+            fire();
+        }
+*/
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -1;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 1;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -1;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 1;
+        }
     }
-    
-    
+
+    /*
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }*/
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
 }
