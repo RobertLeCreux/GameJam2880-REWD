410c569139269e433c88cecf32851e9c8817b932
astropojke <W0292256@nscc.ca>
2017-02-24 22:12:56 -0400

    stuff sound

--- src/gamejam2880/Sound.java
+++ src/gamejam2880/Sound.java
@@ -1,43 +1,34 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gamejam2880;
-
-import javax.sound.sampled.*;
-import java.io.*;
-import javax.swing.*;
 /**
- *
- * @author kylechapman
+ * 
+ * 
+ * 
  */
-<<<<<<< HEAD
-Sound as1 = AudioSystem.getAudioInputStream(new java.io.FileInputStream("chickenDance.wav"));
-               AudioFormat af = as1.getFormat();
-               Clip clip1 = AudioSystem.getClip();
-               DataLine.Info info = new DataLine.Info(Clip.class, af);
 
-               Line line1 = AudioSystem.getLine(info);
-=======
-public Sound {
-<<<<<<< HEAD
-    //ZAP("zap.aif"),
-    //PEW("pew.aif"),
-    //KAPLOW("kaplow.aif"),
-    //BANG("bang.aif"),
-    //ZEW("zew.aif"),
-    //FLAME("flame.aif");
-=======
->>>>>>> origin/master
+import java.io.File;
+import javax.sound.sampled.*;
 
-               if ( ! line1.isOpen() )
-               {
-                clip1.open(as1);
-                clip1.loop(Clip.LOOP_CONTINUOUSLY);
-                clip1.start();
-               }
+public class Sound {
+    public static void main(String[] args) {        
+        try{
+            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("Beats.wav"));
+            Clip test = AudioSystem.getClip();  
+
+            test.open(ais);
+            test.start();
+
+            while (!test.isRunning())
+                Thread.sleep(10);
+            while (test.isRunning())
+                Thread.sleep(10);
+
+            test.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
+
     /**    
     ZAP("zap.wav"),
     PEW("pew.wav"),
@@ -45,25 +36,4 @@ public Sound {
     BANG("bang.wav"),
     ZEW("zew.wav"),
     FLAME("flame.wav");
-<<<<<<< HEAD
-*/
-  
-}
-=======
->>>>>>> origin/master
-
-  public static void main(String[] args) 
-  throws Exception
-  {
-    // open the sound file as a Java input stream
-    String gongFile = "";
-    InputStream in = new FileInputStream(gongFile);
-
-    // create an audiostream from the inputstream
-    AudioStream audioStream = new AudioStream(in);
-
-    // play the audio clip with the audioplayer class
-    AudioPlayer.player.start(audioStream);
-  }
-}
->>>>>>> origin/master
+    */
